680da86a1563733c3964cf7a45ab79d49edf9c64
dragana-u <draganausovic2@gmail.com>
2024-01-31 23:34:17 +0100

    Modified i2

--- src/Ispitni/vtorKolokviumIspit/i2/BooksTest.java
+++ src/Ispitni/vtorKolokviumIspit/i2/BooksTest.java
@@ -1,14 +1,10 @@
 package Ispitni.vtorKolokviumIspit.i2;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
+
+//Решение со List и implements Comparable<Book>
 class Book implements Comparable<Book>{
     String title;
     String category;
@@ -92,3 +88,96 @@ public class BooksTest {
     }
 }
 
+//Решение со Map и со Comparator.comparing
+//import java.util.*;
+//        import java.util.stream.Collectors;
+//
+//
+//class Book{
+//    String naslov;
+//    String category;
+//    float cena;
+//
+//    public Book(String naslov, String category, float cena) {
+//        this.naslov = naslov;
+//        this.category = category;
+//        this.cena = cena;
+//    }
+//
+//    public String getNaslov() {
+//        return naslov.toLowerCase();
+//    }
+//
+//    public String getCategory() {
+//        return category;
+//    }
+//
+//    public float getCena() {
+//        return cena;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return String.format("%s (%s) %.2f",naslov,category,cena);
+//    }
+//}
+//
+//class BookCollection{
+//    Map<String,Set<Book>> books;
+//
+//    public BookCollection() {
+//        books = new TreeMap<>();
+//    }
+//
+//    public void addBook(Book book){
+//        Set<Book> b = books.get(book.category);
+//        if(b == null){
+//            b = new TreeSet<>(Comparator.comparing(Book::getNaslov).thenComparingDouble(Book::getCena));
+//        }
+//        b.add(book);
+//        books.putIfAbsent(book.category,b);
+//    }
+//    public void printByCategory(String category){
+//        Set<Book> b = books.get(category);
+//        b.forEach(System.out::println);
+//    }
+//    public List<Book> getCheapestN(int n){
+//        return books.values().stream().flatMap(Set::stream).sorted(Comparator.comparingDouble(Book::getCena).thenComparing(Book::getNaslov)).limit(n).collect(Collectors.toList());
+//    }
+//}
+//
+//public class BooksTest {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//        scanner.nextLine();
+//        BookCollection booksCollection = new BookCollection();
+//        Set<String> categories = fillCollection(scanner, booksCollection);
+//        System.out.println("=== PRINT BY CATEGORY ===");
+//        for (String category : categories) {
+//            System.out.println("CATEGORY: " + category);
+//            booksCollection.printByCategory(category);
+//        }
+//        System.out.println("=== TOP N BY PRICE ===");
+//        print(booksCollection.getCheapestN(n));
+//    }
+//
+//    static void print(List<Book> books) {
+//        for (Book book : books) {
+//            System.out.println(book);
+//        }
+//    }
+//
+//    static TreeSet<String> fillCollection(Scanner scanner,
+//                                          BookCollection collection) {
+//        TreeSet<String> categories = new TreeSet<String>();
+//        while (scanner.hasNext()) {
+//            String line = scanner.nextLine();
+//            String[] parts = line.split(":");
+//            Book book = new Book(parts[0], parts[1], Float.parseFloat(parts[2]));
+//            collection.addBook(book);
+//            categories.add(parts[1]);
+//        }
+//        return categories;
+//    }
+//}
