38793003a268b3816c32857dfa3b9f2840025c24
dragana-u <draganausovic2@gmail.com>
2024-02-02 17:24:49 +0100

    k1 vlezna 2024

---
+++ src/Ispitni/kolokviumi2024/k1vlezna/RaceTest.java
@@ -0,0 +1,61 @@
+package Ispitni.kolokviumi2024.k1vlezna;
+
+import java.io.*;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+class TeamRace{
+
+    static void findBestTeam (InputStream is, OutputStream os) throws IOException {
+        PrintWriter pw = new PrintWriter(os);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        List<RaceContestant> raceContestants = br.lines().map(RaceContestant::new).collect(Collectors.toList());
+        AtomicLong sec = new AtomicLong();
+        raceContestants
+                .stream()
+                .sorted(Comparator.comparing(RaceContestant::getTime))
+                .limit(4)
+                .forEach(r -> {
+                    pw.println(r);
+                    sec.addAndGet(r.getTime());
+                });
+        pw.println(LocalTime.ofSecondOfDay(sec.longValue()));
+        pw.flush();
+    }
+
+}
+class RaceContestant{
+    String id;
+    LocalTime start;
+    LocalTime end;
+
+    public RaceContestant(String line) {
+        //ID START_TIME END_TIME
+        String[] parts = line.split("\\s+");
+        id = parts[0];
+        start = LocalTime.parse(parts[1]);
+        end = LocalTime.parse(parts[2]);
+    }
+    public long getTime(){
+        return Duration.between(start,end).getSeconds();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s",id,LocalTime.ofSecondOfDay(getTime()).toString());
+    }
+}
+public class RaceTest {
+
+
+    public static void main(String[] args) {
+        try {
+            TeamRace.findBestTeam(System.in, System.out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
